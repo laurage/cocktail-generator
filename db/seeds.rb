51a09493d9af0c3b9cccfbf678ccc49256f7567d
Laura Pedroni <laurage_@hotmail.com>
2016-07-07 21:56:02 +0200

    data seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+ingredients_attributes = [
+  {
+  name: "lemon"
+  },
+    {
+  name: "rum"
+  },
+    {
+  name: "vodka"
+  },
+    {
+  name: "orange"
+  },
+    {
+  name: "ginger"
+  }
+]
+
+cocktails_attributes = [
+  {
+    name: "Mojito"
+    },
+  {
+    name: "Cuba Libre"
+    },
+  {
+    name: "Pink Lady"
+    },
+  {
+    name: "Old fashioned"
+    },
+  {
+    name: "B52"
+    }
+  ]
+
+ingredients_attributes.each { |params| Ingredient.create!(params)}
+cocktails_attributes.each { |params| Cocktail.create!(params)}
