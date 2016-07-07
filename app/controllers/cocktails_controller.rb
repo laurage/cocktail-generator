668884686f1e24a3dd7641efef8e6be9c4bec9f9
Laura Pedroni <laurage_@hotmail.com>
2016-07-07 22:47:50 +0200

    filling views and controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,13 +1,40 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :destroy]
+  # Read (all)
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  # Read (one)
   def show
   end
 
-  def index
-  end
-
+  # Update
   def edit
   end
 
+  def update
+  end
+
+  # Create
   def new
+  end
+
+  def create
+  end
+
+  # Delete
+  def destroy
+    @cocktail.destroy
+  end
+
+  private
+
+  def set_params
+
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
   end
 end
